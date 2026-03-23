bfadac470c86dba1a8a072a5af312f4ccf864e47
Edison Matos <edison.matos@live.com>
2026-03-23 17:39:23 -0300

    feat: Atualiza msg

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -106,7 +106,7 @@ export const infos = {
   youtubeProfile: "",
 
   whatsappDefaultMessage:
-    "Olá! Vim através do site de vocês e gostaria de tirar umas dúvidas.",
+    "Olá! Vim através do site de vocês e gostaria de falar com uma Advogada Criminalista. Pode me ajudar?",
   whatsChatDefaultMessage:
     "Gostaria de falar conosco agora mesmo? Clque abaixo 👇",
   slogan: "",
